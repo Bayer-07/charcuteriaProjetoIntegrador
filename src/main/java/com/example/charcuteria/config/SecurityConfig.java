d1b8b5c83cc9928b9a8b05cf3101cd8c56490062
Edu Muller <eduardoaum2007@gmail.com>
2026-03-29 00:30:28 -0300

    feat: test for handleProfile

--- src/main/java/com/example/charcuteria/config/SecurityConfig.java
+++ src/main/java/com/example/charcuteria/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/register", "/registerAdmin", "/login", "/loginAdmin", "/user/dashboard", "/user/dashboardAdmin").permitAll()
+                .requestMatchers("/register", "/registerAdmin", "/login", "/loginAdmin", "/user/dashboard", "/user/dashboardAdmin", "/handleProfile", "/teste").permitAll()
 
                 .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
 
