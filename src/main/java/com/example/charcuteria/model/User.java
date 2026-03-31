e136a6388ebafb63ae8b1c5ae911785dadda1fcf
Edu Muller <eduardoaum2007@gmail.com>
2026-03-31 18:55:34 -0300

    feat: mudei a forma de auth, provavelmente da pra tirar os logins, faço dps

--- src/main/java/com/example/charcuteria/model/User.java
+++ src/main/java/com/example/charcuteria/model/User.java
@@ -1,10 +1,16 @@
 package com.example.charcuteria.model;
 
 import java.security.Timestamp;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.charcuteria.enums.UserRoleEnum;
 
-public class User {
+public class User implements UserDetails{
     private Integer id;
     private String name;
     private String email;
@@ -92,4 +98,24 @@ public class User {
         return this;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // eu vou retornar true por preguiça, talvez da pra fazer alguma logica nisso dps
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
