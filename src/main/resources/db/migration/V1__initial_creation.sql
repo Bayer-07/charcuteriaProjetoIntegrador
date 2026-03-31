98d423a1832efbeb9a5aabfe1328165498972804
Edu Muller <eduardoaum2007@gmail.com>
2026-03-31 17:15:11 -0300

    feat: categories and subs_plans added

--- src/main/resources/db/migration/V1__initial_creation.sql
+++ src/main/resources/db/migration/V1__initial_creation.sql
@@ -1,3 +1,17 @@
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    description TEXT
+);
+
+CREATE TABLE subscription_plans (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -15,56 +29,35 @@ CREATE TABLE addresses (
     complement VARCHAR(100),
     neighborhood VARCHAR(100),
     city VARCHAR(100) NOT NULL,
-    state VARCHAR(2) NOT NULL,
+    state CHAR(2) NOT NULL,
     zip_code VARCHAR(9) NOT NULL,
     is_default BOOLEAN DEFAULT FALSE,
 
     CONSTRAINT fk_user_address FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE subscription (
+CREATE TABLE subscriptions (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    plan_type VARCHAR(50) NOT NULL,
-    status VARCHAR(20) NOT NULL,
+    plan_id INTEGER NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
     started_at DATE NOT NULL DEFAULT CURRENT_DATE,
 
-    CONSTRAINT fk_user_subscription FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
-
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    address_id INTEGER NOT NULL,
-    total_amount DECIMAL(10,2) NOT NULL,
-    shipping_cost DECIMAL(10,2) NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT fk_orders_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT fk_orders_address FOREIGN KEY (address_id) REFERENCES addresses(id) ON DELETE CASCADE
+    CONSTRAINT fk_user_subscription FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_plan_subscription FOREIGN KEY (plan_id) REFERENCES subscription_plans(id)
 );
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
+    category_id INTEGER NOT NULL,
     name VARCHAR(100) NOT NULL,
     description TEXT,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER DEFAULT 0,
     image_path TEXT,
-    is_active BOOLEAN DEFAULT FALSE
-);
+    is_active BOOLEAN DEFAULT TRUE,
 
-CREATE TABLE order_products (
-    id SERIAL PRIMARY KEY,
-    order_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    unit_price DECIMAL(10,2),
-
-    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
-    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id),
-    CONSTRAINT uq_order_product UNIQUE (order_id, product_id)
+    CONSTRAINT fk_product_category FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE cart_items (
@@ -77,4 +70,29 @@ CREATE TABLE cart_items (
     CONSTRAINT fk_cart_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT fk_cart_product FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
     CONSTRAINT uq_user_product_cart UNIQUE (user_id, product_id)
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    address_id INTEGER NOT NULL,
+    total_amount DECIMAL(10,2) NOT NULL,
+    shipping_cost DECIMAL(10,2) NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_orders_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_orders_address FOREIGN KEY (address_id) REFERENCES addresses(id)
+);
+
+CREATE TABLE order_products (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id),
+    CONSTRAINT uq_order_product UNIQUE (order_id, product_id)
 );
