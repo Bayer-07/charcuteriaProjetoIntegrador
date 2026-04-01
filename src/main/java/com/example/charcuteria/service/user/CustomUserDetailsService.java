83a410c8de12e85fcf0604d130b622e6d9f59859
Edu Muller <eduardoaum2007@gmail.com>
2026-04-01 12:43:38 -0300

    feat: admin-dashboard done

--- src/main/java/com/example/charcuteria/service/user/CustomUserDetailsService.java
+++ src/main/java/com/example/charcuteria/service/user/CustomUserDetailsService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.example.charcuteria.repository.UserRepository;
+import com.example.charcuteria.repository.user.UserRepository;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
