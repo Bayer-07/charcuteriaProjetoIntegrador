851fb05bf66c55f2f006ac71773e35b8a208d5e7
Edu Muller <eduardoaum2007@gmail.com>
2026-04-22 19:48:03 -0300

    clean: clean files

--- src/main/java/com/example/charcuteria/service/category/CategoryService.java
+++ src/main/java/com/example/charcuteria/service/category/CategoryService.java
@@ -6,10 +6,8 @@ import org.springframework.stereotype.Service;
 import com.example.charcuteria.dto.category.CategoryEditRequestDto;
 import com.example.charcuteria.dto.category.CategoryEditResponseDto;
 import com.example.charcuteria.dto.category.CategoryRequestDto;
-import com.example.charcuteria.dto.category.CategoryResponseDto;
 import com.example.charcuteria.exceptions.BusinessException;
 import com.example.charcuteria.exceptions.ProductErrorCode;
-import com.example.charcuteria.model.Category;
 import com.example.charcuteria.repository.category.CategoryRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -17,34 +15,27 @@ import jakarta.persistence.EntityNotFoundException;
 @Service
 public class CategoryService {
 
-    private final CategoryRepository repository;
+    private final CategoryRepository categoryRepository;
 
-    public CategoryService(CategoryRepository repository) {
-        this.repository = repository;
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
     }
 
     public CategoryEditResponseDto getById(Integer id) {
-        return repository.getById(id);
-    }
-
-    public CategoryResponseDto returnByName(String name) {
-        Category category = repository.findByName(name)
-        .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
-
-        return toDTO(category);
+        return categoryRepository.getById(id);
     }
 
     public void createCategory(CategoryRequestDto category) {
-        if (repository.createCategory(category) == 0 ) throw new BusinessException(ProductErrorCode.PRODUCT_NOT_FOUND);
+        if (categoryRepository.createCategory(category) == 0 ) throw new BusinessException(ProductErrorCode.PRODUCT_NOT_FOUND);
     }
 
     public void updateCategoryById(CategoryEditRequestDto category) {
-        if (repository.updateCategoryById(category) == 0) throw new BusinessException(ProductErrorCode.PRODUCT_NOT_FOUND);
+        if (categoryRepository.updateCategoryById(category) == 0) throw new BusinessException(ProductErrorCode.PRODUCT_NOT_FOUND);
     }
 
     public void deleteById(Integer id) {
         try {
-            int rows = repository.delete(id);
+            int rows = categoryRepository.deleteById(id);
             if (rows == 0) {
                 throw new EntityNotFoundException("Category with ID " + id + " not found");
             }
@@ -57,11 +48,4 @@ public class CategoryService {
         }
     }
 
-    // Mappers de Dto e Entity
-    private CategoryResponseDto toDTO(Category category) {
-        CategoryResponseDto dto = new CategoryResponseDto();
-        dto.setName(category.getName());
-        dto.setDescription(category.getDescription());
-        return dto;
-    }
 }
