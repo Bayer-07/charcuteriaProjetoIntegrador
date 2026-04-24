6bd6ba08972d0fa36aab3201991c8e71db9ff1df
Bayer-07 <gabrielbayer2603@gmail.com>
2026-04-23 21:41:57 -0300

    feat G2-23: Criada a página index, com: carrossel de imagens no topo da página, 4 produtos mais vendidos, link para aba de assinaturas e link para aba de parcerias. Também foi adicionado o endpoint index/top-products para retornar os 4 produtos mais vendidos pela empresa e mostrá-los na página inicial

--- src/main/java/com/example/charcuteria/controller/IndexController.java
+++ src/main/java/com/example/charcuteria/controller/IndexController.java
@@ -1,14 +1,37 @@
 package com.example.charcuteria.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.charcuteria.dto.product.TopProductResponseDto;
+import com.example.charcuteria.service.product.ProductService;
 
 @Controller
 @RequestMapping
 public class IndexController {
+    private final ProductService productService;
+
+    public IndexController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
     public String Index() {
         return "public/index";
+    }
+
+    @GetMapping("/index")
+    public String getIndex() {
+        return "redirect:public/index";
+    }
+
+    @GetMapping("/index/top-products")
+    @ResponseBody
+    public List<TopProductResponseDto> getTopPurchasedProducts() {
+        return productService.getTopPurchasedProducts(4);
     }
 }
