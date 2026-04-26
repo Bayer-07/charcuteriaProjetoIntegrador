9b352c55fd8cb30b2611af8f72651efe7a0cce12
Edu Muller <eduardoaum2007@gmail.com>
2026-04-26 17:34:52 -0300

    feat: tela de cart pronta

--- src/main/java/com/example/charcuteria/service/cart/CartService.java
+++ src/main/java/com/example/charcuteria/service/cart/CartService.java
@@ -1,8 +1,11 @@
 package com.example.charcuteria.service.cart;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.charcuteria.dto.cart.CartResponseDto;
 import com.example.charcuteria.exceptions.BusinessException;
 import com.example.charcuteria.exceptions.ProductErrorCode;
 import com.example.charcuteria.repository.cart.CartRepository;
@@ -21,4 +24,7 @@ public class CartService {
         if(cartRepository.addCartItem(productId, userId) == 0) throw new BusinessException(ProductErrorCode.PRODUCT_NOT_FOUND);
     }
 
+    public List<CartResponseDto> getAllCartItems(Integer userId) {
+        return cartRepository.findAllByUserId(userId);
+    }
 }
