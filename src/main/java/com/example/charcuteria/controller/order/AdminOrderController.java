1f06ff30971be3704a7c6dad60d79478aefa6f14
Julio <julincapixaba@gmail.com>
2026-05-28 21:33:27 -0300

    feat: Implementação da tela Pedidos Admin

---
+++ src/main/java/com/example/charcuteria/controller/order/AdminOrderController.java
@@ -0,0 +1,28 @@
+package com.example.charcuteria.controller.order;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.charcuteria.model.User;
+import com.example.charcuteria.service.order.OrderService;
+
+@Controller
+@RequestMapping("/admin/orders")
+public class AdminOrderController {
+    
+    private final OrderService orderService;
+
+    public AdminOrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public String returnAll(Model model) {
+        model.addAttribute("orders", orderService.findAll());
+        return "/admin/orders";
+    }
+}
