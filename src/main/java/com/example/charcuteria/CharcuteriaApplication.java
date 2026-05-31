21fa5088c4a61789ef12fc8e2ef22c7402682b81
Julio <julincapixaba@gmail.com>
2026-05-31 14:25:39 -0300

    feat: botão de visualização

--- src/main/java/com/example/charcuteria/CharcuteriaApplication.java
+++ src/main/java/com/example/charcuteria/CharcuteriaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CharcuteriaApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(CharcuteriaApplication.class, args);
 	}
